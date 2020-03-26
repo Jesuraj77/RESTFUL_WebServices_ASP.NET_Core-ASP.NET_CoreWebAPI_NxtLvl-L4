fe87bac244ec49b5ae25481428824b6284c957b2
itdev-lenovo <it1devops@gmail.com>
2020-03-26 20:25:00 +0530

    files added

---
+++ Level4/Controllers/NewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Level4.Controllers
+{
+    public class NewController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Add XML formatters to the API and send and receive data as JSON and XML and Add the documentation tool, Swagger and add the parameters and enable logging. Extend the get method and filter entries with String
+            return View();
+        }
+    }
+}
